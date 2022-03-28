f82fe88b05210a566a07660b3d3e1b15975ece81
Michael Hudson <Michael.Hudson@wearetipjar.com>
2022-03-28 18:43:42 +0200

    implement WIP home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { SentencesModule } from './modules/sentences/sentences.module';
 import { SharedModule } from './modules/shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
